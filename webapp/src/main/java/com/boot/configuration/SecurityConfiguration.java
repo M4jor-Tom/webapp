57095b4bc88cd2e082f1f90408fdd3620a6cb4a3
johan <johan@boot>
2020-09-20 14:35:14 +0200

    Configuration simple de la securite

--- webapp/src/main/java/com/boot/configuration/SecurityConfiguration.java
+++ webapp/src/main/java/com/boot/configuration/SecurityConfiguration.java
@@ -1,17 +1,43 @@
 package com.boot.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/console/*")
-				.permitAll();
+		http.authorizeRequests()
+			.antMatchers("/product/edit/*","/product/new","/product/delete/*","/product/show/*").authenticated()	
+			.anyRequest().permitAll()
+			.and().formLogin().loginPage("/login").permitAll()
+			.and().logout().permitAll();
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
+	}
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("user")
+				.roles("USER")
+				.build();
+		UserDetails admin =
+				User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("admin")
+				.roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user,admin);
 	}
 }
