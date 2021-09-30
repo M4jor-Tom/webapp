eadd2593465ad8159f03c0ef98cc89bd6dca59c4
Tom <tom-vautray@hotmail.fr>
2021-09-30 11:44:06 +0200

    Added class ProductController mapped from class ProductService
    map
    
    
    mapping

---
+++ webapp/src/main/java/com/boot/controllers/ProductController.java
@@ -0,0 +1,54 @@
+package com.boot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.boot.entities.Product;
+import com.boot.service.ProductService;
+
+@Controller
+public class ProductController {
+     private ProductService productService;
+
+     @RequestMapping(value = "products", method = RequestMethod.GET)
+     public String list(Model model){
+    	 model.addAttribute("products", productService.listAllProducts());
+    	 return "products";
+     }
+     
+     @RequestMapping(value = "product/show/{id}", method = RequestMethod.GET)
+     public String showProduct(@PathVariable Integer id, Model model){
+    	 model.addAttribute("product", productService.getProductById(id));
+    	 return "productshow";
+     }
+     
+     @RequestMapping(value = "product/edit/{id}", method = RequestMethod.PUT)
+     public String edit(@PathVariable Integer id, Model model){
+    	 model.addAttribute(
+    			 "product",
+    			 null	//new ProductForm()
+    		);
+    	 return "productform";
+     }
+     
+     @RequestMapping(value = "product/new", method = RequestMethod.POST)
+     public String newProduct(Model model){
+    	 model.addAttribute("product", new Product());
+    	 return "productform";
+     }
+
+     @RequestMapping(value = "product", method = RequestMethod.POST)
+     public String saveProduct(Product product){
+    	 productService.saveProduct(product);
+    	 return "redirect:/product/show/" + product.getId();
+     }
+     
+     @RequestMapping(value = "product/delete/{id}", method = RequestMethod.DELETE)
+     public String delete(@PathVariable Integer id){
+    	 productService.deleteProduct(id);
+    	 return "redirect:/products";
+     }
+}
