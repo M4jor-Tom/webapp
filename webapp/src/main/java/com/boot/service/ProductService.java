5e0bc0db3824278ca0d443e283afe9a45b4c4e64
Tom <tom-vautray@hotmail.fr>
2021-09-30 10:55:53 +0200

    Added interface ProductService

---
+++ webapp/src/main/java/com/boot/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.boot.service;
+
+import com.boot.entities.Product;
+
+public interface ProductService {
+    Iterable<Product> listAllProducts();
+
+    Product getProductById(Integer id);
+
+    Product saveProduct(Product product);
+
+    void deleteProduct(Integer id);
+}
