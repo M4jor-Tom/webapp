da7b0b0aedcc8f8c6a99fbbd49d989f50262943b
Tom <tom-vautray@hotmail.fr>
2021-09-30 14:52:40 +0200

    Added SecurityConfiguration.userDetailsService()

--- webapp/src/main/java/com/boot/configuration/SecurityConfiguration.java
+++ webapp/src/main/java/com/boot/configuration/SecurityConfiguration.java
@@ -1,8 +1,13 @@
 package com.boot.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -21,5 +26,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	        	.logout().permitAll()
 			.and()
 				.csrf().disable();
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("mdp")
+				.roles("USER")
+				.build();
+		UserDetails admin =
+				User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("mdp")
+				.roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user, admin);
 	}
 }
