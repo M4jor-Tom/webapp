969430a1918e8d20e47ca852b9fc019ff49a34d0
Tom <tom-vautray@hotmail.fr>
2021-09-30 11:40:02 +0200

    Added class ProductFrom

---
+++ webapp/src/main/java/com/boot/forms/ProductForm.java
@@ -0,0 +1,34 @@
+package com.boot.forms;
+
+public class ProductForm {
+
+    private String productId;
+    private String description;
+    private String imageUrl;
+    private float price;
+    
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getProductId() {
+        return productId;
+    }
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
