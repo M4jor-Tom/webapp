4de3e9b141f3e90cb38656c3ea92eb34b343210e
Tom <tom-vautray@hotmail.fr>
2021-09-30 16:42:32 +0200

    Made interface ProductRepository pretty

--- webapp/src/main/java/com/boot/repositories/ProductRepository.java
+++ webapp/src/main/java/com/boot/repositories/ProductRepository.java
@@ -1,7 +1,8 @@
 package com.boot.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
 import com.boot.entities.Product;
+
 public interface ProductRepository extends CrudRepository<Product, Integer>{
+	
 }
