8cb13c0fbda2dc9412ef2a5ab31ea08d472d8c8d
Tom <tom-vautray@hotmail.fr>
2021-09-30 12:02:24 +0200

    Reindented class ProductController

--- webapp/src/main/java/com/boot/controllers/ProductController.java
+++ webapp/src/main/java/com/boot/controllers/ProductController.java
@@ -12,42 +12,39 @@ import com.boot.service.ProductService;
 
 @Controller
 public class ProductController {
-     private ProductService productService;
+	private ProductService productService;
 
-     @RequestMapping(value = "products", method = RequestMethod.GET)
-     public String list(Model model){
-    	 model.addAttribute("products", productService.listAllProducts());
-    	 return "products";
-     }
-     
-     @RequestMapping(value = "product/show/{id}", method = RequestMethod.GET)
-     public String showProduct(@PathVariable Integer id, Model model){
-    	 model.addAttribute("product", productService.getProductById(id));
-    	 return "productshow";
-     }
-     
-     @RequestMapping(value = "product/edit/{id}", method = RequestMethod.PUT)
-     public String edit(@PathVariable Integer id, Model model){
-    	 model.addAttribute(
-    			 "product",
-    			 new ProductForm()
-    		);
-    	 return "productform";
-     }
-     
-     @RequestMapping(value = "product/new", method = RequestMethod.POST)
-     public String newProduct(Model model){
-    	 model.addAttribute("product", new Product());
-    	 return "productform";
-     }
+	@RequestMapping(value = "products", method = RequestMethod.GET)
+	public String list(Model model) {
+		model.addAttribute("products", productService.listAllProducts());
+		return "products";
+	}
 
-     @RequestMapping(value = "product", method = RequestMethod.POST)
-     public String saveProduct(Product product){
-    	 productService.saveProduct(product);
-    	 return "redirect:/product/show/" + product.getId();
-     }
-     
-     private ProductService getProductService() {
+	@RequestMapping(value = "product/show/{id}", method = RequestMethod.GET)
+	public String showProduct(@PathVariable Integer id, Model model) {
+		model.addAttribute("product", productService.getProductById(id));
+		return "productshow";
+	}
+
+	@RequestMapping(value = "product/edit/{id}", method = RequestMethod.PUT)
+	public String edit(@PathVariable Integer id, Model model) {
+		model.addAttribute("product", new ProductForm());
+		return "productform";
+	}
+
+	@RequestMapping(value = "product/new", method = RequestMethod.POST)
+	public String newProduct(Model model) {
+		model.addAttribute("product", new Product());
+		return "productform";
+	}
+
+	@RequestMapping(value = "product", method = RequestMethod.POST)
+	public String saveProduct(Product product) {
+		productService.saveProduct(product);
+		return "redirect:/product/show/" + product.getId();
+	}
+
+	private ProductService getProductService() {
 		return productService;
 	}
 
@@ -56,8 +53,8 @@ public class ProductController {
 	}
 
 	@RequestMapping(value = "product/delete/{id}", method = RequestMethod.DELETE)
-     public String delete(@PathVariable Integer id){
-    	 productService.deleteProduct(id);
-    	 return "redirect:/products";
-     }
+	public String delete(@PathVariable Integer id) {
+		productService.deleteProduct(id);
+		return "redirect:/products";
+	}
 }
