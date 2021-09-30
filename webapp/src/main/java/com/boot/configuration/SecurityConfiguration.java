697fe1936e6bab0088231a092bd9add5f23a64c1
Tom <tom-vautray@hotmail.fr>
2021-09-30 15:15:54 +0200

    Added defaultSuccessUrl() to index

--- webapp/src/main/java/com/boot/configuration/SecurityConfiguration.java
+++ webapp/src/main/java/com/boot/configuration/SecurityConfiguration.java
@@ -22,6 +22,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.anyRequest().permitAll()
 	        .and()
 	        	.formLogin().loginPage("/login").permitAll()
+		        .defaultSuccessUrl("/index")
 	        .and()
 	        	.logout().permitAll()
 			.and()
