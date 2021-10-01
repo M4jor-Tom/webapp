0cb2dd61efd673bebc4672e25131b067dc0c03ac
Tom <tom-vautray@hotmail.fr>
2021-10-01 08:26:07 +0200

    Added interface CRUDService

---
+++ webapp/src/main/java/com/boot/service/CRUDService.java
@@ -0,0 +1,13 @@
+package com.boot.service;
+
+import java.util.List;
+
+public interface CRUDService<T> {
+    List<?> listAll();
+
+    T getById(Integer id);
+
+    T saveOrUpdate(T domainObject);
+
+    void delete(Integer id);
+}
