49db82daddfb8a3947d73a51be460db8bdbf00b3
Tom <tom-vautray@hotmail.fr>
2021-10-01 11:05:11 +0200

    Added class CommonBeanConfig
    CommonBeanConfig padding removed

---
+++ webapp/src/main/java/com/boot/configuration/CommonBeanConfig.java
@@ -0,0 +1,14 @@
+package com.boot.configuration;
+
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonBeanConfig {
+    @Bean
+    public StrongPasswordEncryptor strongEncryptor() {
+        StrongPasswordEncryptor encryptor = new StrongPasswordEncryptor();
+        return encryptor;
+    }
+}
