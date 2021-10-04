69bfbfdf40e3588ba8df4b8896fb82d01bf2f9bb
Tom <tom-vautray@hotmail.fr>
2021-10-04 14:32:12 +0200

    interface IController now only expects a DomainObject as type parameter

--- webapp/src/main/java/com/boot/controllers/IController.java
+++ webapp/src/main/java/com/boot/controllers/IController.java
@@ -3,9 +3,9 @@ package com.boot.controllers;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.boot.domain.AbstractDomainClass;
+import com.boot.domain.DomainObject;
 
-public interface IController<T extends AbstractDomainClass> {
+public interface IController<T extends DomainObject> {
 	public String list(Model model);
 
 	public String show(@PathVariable Integer id, Model model);
@@ -14,7 +14,7 @@ public interface IController<T extends AbstractDomainClass> {
 
 	public String newObject(Model model);
 
-	public String save(AbstractDomainClass object);
+	public String save(DomainObject domainObject);
 
 	public String delete(@PathVariable Integer id);
 }
