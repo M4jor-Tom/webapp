827d164ed206ac1a0d4488a1d734206392b7e495
Tom <tom-vautray@hotmail.fr>
2021-10-04 14:27:43 +0200

    Renamed interface Controller to IController and removed Service get/set
    import

--- webapp/src/main/java/com/boot/controllers/IController.java
+++ webapp/src/main/java/com/boot/controllers/IController.java
@@ -4,9 +4,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.boot.domain.AbstractDomainClass;
-import com.boot.services.CRUDService;
 
-public interface Controller<T extends AbstractDomainClass> {
+public interface IController<T extends AbstractDomainClass> {
 	public String list(Model model);
 
 	public String show(@PathVariable Integer id, Model model);
@@ -16,10 +15,6 @@ public interface Controller<T extends AbstractDomainClass> {
 	public String newObject(Model model);
 
 	public String save(AbstractDomainClass object);
-
-	public CRUDService<T> getCRUDService();
-
-	public void setService(CRUDService<T> crudService);
 
 	public String delete(@PathVariable Integer id);
 }
